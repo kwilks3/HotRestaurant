fd362e237843de480c5a40b6e0afe15f926abcaf
Tanner Griffin <tannermgriffin@gmail.com>
2020-07-02 21:10:04 -0500

    Added ajax get and posts

--- server.js
+++ server.js
@@ -1,49 +1,123 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-var tablesApi = [];
-var waitlistApi = [];
+var tablesApi = [
+	// {
+	// 	customerName: 'dsadsa',
+	// 	phoneNumber: 'gdfgdf',
+	// 	customerEmail: 'gfdgd',
+	// 	customerID: 'gfdgd',
+	// },
+	// {
+	// 	customerName: 'dsadsa',
+	// 	phoneNumber: 'gdfgdf',
+	// 	customerEmail: 'gfdgd',
+	// 	customerID: 'gfdgd',
+	// },
+	// {
+	// 	customerName: 'dsadsa',
+	// 	phoneNumber: 'gdfgdf',
+	// 	customerEmail: 'gfdgd',
+	// 	customerID: 'gfdgd',
+	// },
+	// {
+	// 	customerName: 'dsadsa',
+	// 	phoneNumber: 'gdfgdf',
+	// 	customerEmail: 'gfdgd',
+	// 	customerID: 'gfdgd',
+	// },
+	// {
+	// 	customerName: 'dsadsa',
+	// 	phoneNumber: 'gdfgdf',
+	// 	customerEmail: 'gfdgd',
+	// 	customerID: 'gfdgd',
+	// },
+];
+var waitlistApi = [
+    {
+		customerName: 'dsadsa',
+		phoneNumber: 'gdfgdf',
+		customerEmail: 'gfdgd',
+		customerID: 'gfdgd',
+	},
+	{
+		customerName: 'dsadsa',
+		phoneNumber: 'gdfgdf',
+		customerEmail: 'gfdgd',
+		customerID: 'gfdgd',
+	},
+	{
+		customerName: 'dsadsa',
+		phoneNumber: 'gdfgdf',
+		customerEmail: 'gfdgd',
+		customerID: 'gfdgd',
+	},
+	{
+		customerName: 'dsadsa',
+		phoneNumber: 'gdfgdf',
+		customerEmail: 'gfdgd',
+		customerID: 'gfdgd',
+	},
+	{
+		customerName: 'dsadsa',
+		phoneNumber: 'gdfgdf',
+		customerEmail: 'gfdgd',
+		customerID: 'gfdgd',
+	},
+];
 
 const port = process.env.PORT || 8000;
 
 // view routes
-app.get("/", (req, res) => {
-    console.log("home");
-    res.sendFile(path.join(__dirname, "index.html"));
+app.get('/', (req, res) => {
+	console.log('home');
+	res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get("/tables", (req, res) => {
-    console.log("tables");
-    res.sendFile(path.join(__dirname, "tables.html"));
+app.get('/tables', (req, res) => {
+	console.log('tables');
+	res.sendFile(path.join(__dirname, 'tables.html'));
 });
 
-app.get("/reservations", (req, res) => {
-    console.log("reservations");
-    res.sendFile(path.join(__dirname, "reservation.html"));
+app.get('/reservations', (req, res) => {
+	console.log('reservations');
+	res.sendFile(path.join(__dirname, 'reservation.html'));
 });
 
 // api post routes
-app.post("/waitlistapi", (req, res) => {
-    res.json(waitlistApi);
-});
+// app.post('/api/waitlist', (req, res) => {
+// 	var newWaitlist = req.body;
+// 	newWaitlist.routeName = newWaitlist.name.replace(/\s+/g, '').toLowerCase();
+// 	console.log(newWaitlist);
+// 	waitlistApi.push(newWaitlist);
+// 	res.json(waitlistApi);
+// });
 
-app.post("/tablesapi", (req, res) => {
-    res.json(tablesApi);
+app.post('/api/tables', (req, res) => {
+	var newTables = req.body;
+    console.log(newTables);
+    if (tablesApi.length >= 5) {
+        waitlistApi.push(newTables);
+        res.send("Our 5 tables are taken, you're on the waitlist!");
+    } else {
+        tablesApi.push(newTables);
+        res.send("You're on the list!");
+    }
 });
 
 // api get routes
-app.get("/waitlistapi", (req, res) => {
-    res.sendFile(path.join(__dirname, "reservation.html"));
+app.get('/api/waitlist', (req, res) => {
+	res.json(waitlistApi);
 });
 
-app.get("/tablesapi", (req, res) => {
-    res.sendFile(path.join(__dirname, "reservation.html"));
+app.get('/api/tables', (req, res) => {
+	res.json(tablesApi);
 });
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+	console.log(`Listening on port ${port}`);
 });
