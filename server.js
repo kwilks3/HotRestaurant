5c8ad3e5d30c6a27089aaeee690fd48f03a21d22
Andrew Venson <vensoneandrew@gmail.com>
2020-07-02 19:59:52 -0500

    added server routes

--- server.js
+++ server.js
@@ -1,1 +1,29 @@
 const express = require("express");
+const path = require("path");
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+var tables = [];
+var waiting = [];
+
+const port = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+    console.log("home");
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/tables", (req, res) => {
+    console.log("tables");
+    res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+app.get("/reservations", (req, res) => {
+    console.log("reservations");
+    res.sendFile(path.join(__dirname, "reservation.html"));
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
